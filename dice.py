e339c07560f717053fddafe4e4d08c08abda4e9f
TroyGrorud7 <150355125+TroyGrorud7@users.noreply.github.com>
2025-10-03 21:12:02 +0000

    First_Dice_Code

--- dice.py
+++ dice.py
@@ -1,2 +1,16 @@
-#This file name should be Dice.py 
-print("Hello World")
+import random
+#This file will need more work....
+six_side_di=[1,2,3,4,5,6]
+dealer_score=[]
+player_score=[]
+
+
+
+
+def player_hit():
+    random.choice(six_side_di)
+    random.choice(six_side_di)
+
+def dealer_hit():
+    random.choice(six_side_di)
+
